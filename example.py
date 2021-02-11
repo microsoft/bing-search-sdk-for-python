5405e6f4e891ca696b2d024c709b3ff69924e97b
unknown <a-maal@microsoft.com>
2021-02-11 14:52:48 +0200

    fixing imports

--- example.py
+++ example.py
@@ -12,9 +12,6 @@ import pkgutil
 
 import tools
 import os,sys,inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
 import samples.sdk
 
 
